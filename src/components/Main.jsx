c4df5faacee3e2223c88895a1af5679019d42fa6
juniorMira <j-unyor14@hotmail.com>
2023-10-31 22:29:47 -0300

    create main with carrousel

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,31 @@
-import Banner from '../images/BannerWebsite.png';
 import './Main.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import Carousel from 'react-bootstrap/Carousel';
+import React from 'react';
+
+import Banner1 from '../images/Banner1.png';
+import Banner2 from '../images/Banner2.png';
+import Banner3 from '../images/Banner3.png';
+import Banner4 from '../images/Banner4.png';
+
+const images = [Banner1, Banner2, Banner3, Banner4];
 
 export default function Main() {
+  const renderCarouselItems = () => {
+    return images.map((banner, index) => (
+      <Carousel.Item key={index} interval={1500}>
+        <img
+          className="d-block w-100"
+          src={banner}
+          alt={`Banner${index + 1}`}
+        />
+      </Carousel.Item>
+    ));
+  };
+
   return (
     <section className="showcase-area">
-      <div className="container">
-        <div className="showcase-image">
-          <img src={Banner} className="person" alt="banner" />
-        </div>
-      </div>
+      <Carousel fade>{renderCarouselItems()}</Carousel>
     </section>
   );
 }
